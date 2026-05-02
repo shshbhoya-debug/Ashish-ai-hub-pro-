9a27f00c74c6ac1310a5ba4411eddaf1a2d8bcad
shshbhoya-debug <shshbhoya@gmail.com>
2026-05-02 08:33:32 +0000

    Navigation: Connected Image Generator screen

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Screens import kar rahe hain
+// Screens imports
 import HomeScreen from './src/screens/HomeScreen';
 import ChatScreen from './src/screens/ChatScreen';
+import ImageGenScreen from './src/screens/ImageGenScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,12 +15,13 @@ export default function App() {
       <Stack.Navigator 
         initialRouteName="Home"
         screenOptions={{
-          headerShown: false, // Humne apna custom header banaya hai
+          headerShown: false,
           cardStyle: { backgroundColor: '#fff' }
         }}
       >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="ChatScreen" component={ChatScreen} />
+        <Stack.Screen name="ImageGenScreen" component={ImageGenScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
